51383e092cc276709c5dc85bd11d0c7da4f49abf
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2022-07-03 15:55:57 +0200

    Finished v1.0

---
+++ Calculator 1.0/app.js
@@ -0,0 +1,25 @@
+let input1 = document.getElementById('num1');
+let input2 = document.getElementById('num2');
+let result = document.getElementById('result');
+
+document.getElementById('add').addEventListener('click', () => {
+    result.value = +input1.value + +input2.value;
+});
+
+document.getElementById('sub').addEventListener('click', () => {
+    result.value = +input1.value - +input2.value;
+});
+
+document.getElementById('mult').addEventListener('click', () => {
+    result.value = +input1.value * +input2.value;
+});
+
+document.getElementById('div').addEventListener('click', () => {
+    result.value = +input1.value / +input2.value;
+});
+
+document.getElementById('clear').addEventListener('click', () => {
+    input1.value = '';
+    input2.value = '';
+    result.value = '';
+});
