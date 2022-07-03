da3a7b0c2dbdda61449e337725b9dbe8cf73b366
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2022-07-04 00:31:01 +0200

    v1.0

--- Calculator 1.0/app.js
+++ Calculator 1.0/app.js
@@ -2,21 +2,33 @@ let input1 = document.getElementById('num1');
 let input2 = document.getElementById('num2');
 let result = document.getElementById('result');
 
+
 document.getElementById('add').addEventListener('click', () => {
-    result.value = +input1.value + +input2.value;
+      
+        result.value = Number(input1.value) + Number(input2.value);
 });
+    
 
 document.getElementById('sub').addEventListener('click', () => {
-    result.value = +input1.value - +input2.value;
+    result.value = Number(input1.value) - Number(input2.value);
 });
+
 
 document.getElementById('mult').addEventListener('click', () => {
-    result.value = +input1.value * +input2.value;
+    result.value = Number(input1.value) * Number(input2.value);
 });
 
+
 document.getElementById('div').addEventListener('click', () => {
-    result.value = +input1.value / +input2.value;
+
+    if(Number(input2.value) == 0){
+        result.value = 'Undefined';
+    }else{
+        result.value = Number(input1.value) / Number(input2.value);
+    }
+    
 });
+
 
 document.getElementById('clear').addEventListener('click', () => {
     input1.value = '';
